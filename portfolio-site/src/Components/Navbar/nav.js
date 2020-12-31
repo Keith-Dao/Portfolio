8355f63b295f04c7c9b4e1b7d12ab6fc3fbf435d
Keith <keith.dao.248@gmail.com>
2020-12-31 13:56:54 +1000

    Changed nav bar to be a sidebar

--- portfolio-site/src/Components/Navbar/nav.js
+++ portfolio-site/src/Components/Navbar/nav.js
@@ -4,14 +4,11 @@ import './nav.css';
 function NavBar(){
     return (
         <nav id="main-navbar">
-            <div className = "logo">Keith Dao</div> {/* Change this to an image later*/}
-            <div className = "nav-links-container">
-                <ul className = "nav-links">
-                    <li>Link 1</li>
-                    <li>Link 2</li>
-                    <li>Link 3</li>
-                </ul>
-            </div>
+            <ul className = "nav-links">
+                <li>Link 1</li>
+                <li>Link 2</li>
+                <li>Link 3</li>
+            </ul>
         </nav>
     );
 }
