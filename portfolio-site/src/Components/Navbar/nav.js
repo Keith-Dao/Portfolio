a626b8a5cddb864eade5000db8a7c01699dbd844
Keith <keith.dao.248@gmail.com>
2021-01-22 17:46:18 +1000

    Added nav menu for mobile

--- portfolio-site/src/Components/Navbar/nav.js
+++ portfolio-site/src/Components/Navbar/nav.js
@@ -4,10 +4,19 @@ import './nav.css';
 const NavBar = (props) => {
     return (
     <nav id="main-navbar">
+        <label id="nav-hamburger-wrapper"> 
+            <input type="checkbox" id="nav-hamburger" onChange={() => {
+                document.getElementById('main-navbar').classList.toggle('show');
+                document.body.classList.toggle('scroll-lock')
+            }}></input>
+        </label>
         <div id="nav-links">
             {props.sections.map((section) => 
                 <a href={section.link != null ? section.link : `#${section.header.replace(' ', () => '-')}`} key={section.header}> 
-                    <button key={section.header} type="button">
+                    <button key={section.header} type="button" onClick={() => {
+                        document.getElementById('nav-links').classList.remove('show')
+                        document.body.classList.remove('scroll-lock')
+                    }}>
                         {section.header.replace(/\b\w/g, l => l.toUpperCase())}
                     </button>
                 </a>
