1751d203a36aa5ec5dedac9c1d7180a64ba387cc
Keith <keith.dao.248@gmail.com>
2021-01-23 15:25:47 +1000

    Utilised states to toggle mobile nav

--- portfolio-site/src/Components/Navbar/nav.js
+++ portfolio-site/src/Components/Navbar/nav.js
@@ -1,22 +1,24 @@
 import React from 'react';
+import { useState } from 'react';
 import './nav.css';
 
 const NavBar = (props) => {
+    const [showNav, toggleNav] = useState(false);
+
+    const toggleNavVisibility = () => {
+        showNav ? document.body.classList.remove('scroll-lock') : document.body.classList.add('scroll-lock');
+        toggleNav((prev) => !prev);
+    }
+
     return (
-    <nav id="main-navbar">
+    <nav id="main-navbar" className={showNav ? 'show' : ''}>
         <label id="nav-hamburger-wrapper"> 
-            <input type="checkbox" id="nav-hamburger" onChange={() => {
-                document.getElementById('main-navbar').classList.toggle('show');
-                document.body.classList.toggle('scroll-lock')
-            }}></input>
+            <input type="checkbox" id="nav-hamburger" onChange={toggleNavVisibility}></input>
         </label>
         <div id="nav-links">
             {props.sections.map((section) => 
                 <a href={section.link != null ? section.link : `#${section.header.replace(' ', () => '-')}`} key={section.header}> 
-                    <button key={section.header} type="button" onClick={() => {
-                        document.getElementById('nav-links').classList.remove('show')
-                        document.body.classList.remove('scroll-lock')
-                    }}>
+                    <button key={section.header} type="button" onClick={toggleNavVisibility}>
                         {section.header.replace(/\b\w/g, l => l.toUpperCase())}
                     </button>
                 </a>
