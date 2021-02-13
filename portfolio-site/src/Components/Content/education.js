298aaf0e2587011cc023e2e6617c5aafe40bf366
Keith <keith.dao.248@gmail.com>
2021-02-13 14:30:21 +1000

    Added education logos

--- portfolio-site/src/Components/Content/education.js
+++ portfolio-site/src/Components/Content/education.js
@@ -1,17 +1,23 @@
 import React from "react";
 import "./education.css";
 
+// Pictures
+import mansfield from "./images/mansfield.png";
+import uq from "./images/uq.png";
+
 // Education list
 const education = [
 	{
 		name: "Secondary Education",
 		location: "Mansfield State High School",
 		years: "2014 - 2018",
+		image: mansfield,
 	},
 	{
 		name: "Bachelor of Computer Science (Machine Learning)",
 		location: "University of Queensland",
 		years: "2019 - present",
+		image: uq,
 	},
 ];
 
@@ -21,9 +27,11 @@ const Course = ({ name, years, location, image }) => {
 		<div className="course-wrapper">
 			<img src={image} className="course-image" alt={`${location} logo`} />
 			<div className="info-wrapper">
-				<h1 className="course-name">{name}</h1>
-				<h2 className="course-location">{location}</h2>
-				<p className="course-duration">{years}</p>
+				<div className="vertical-center">
+					<h1 className="course-name">{name}</h1>
+					<h2 className="course-location">{location}</h2>
+					<p className="course-duration">{years}</p>
+				</div>
 			</div>
 		</div>
 	);
